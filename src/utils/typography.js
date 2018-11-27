7c9fcc3b775149f9e0702428cc3818ae2880340d
Micheal Hickerson <mikehickerson@gmail.com>
2018-11-27 15:28:26 -0500

    Add Lincoln typography theme

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,13 +1,6 @@
 import Typography from 'typography'
+import lincolnTheme from 'typography-theme-lincoln'
 
-const typography = new Typography({
-  baseFontSize: '18px',
-  baseLineHeight: 1.666,
-  headerFontFamily: ['Avenir Next', 'Helvetica Neue', 'Segoe UI', 'Helvetica', 'Arial', 'sans-serif'],
-  bodyFontFamily: ['Georgia', 'serif'],
-})
-
-// Output CSS as string.
-typography.toString()
+const typography = new Typography(lincolnTheme)
 
 export default typography
