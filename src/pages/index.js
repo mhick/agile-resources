09beafbc3e1b110aa18791a77433f62b92199da3
Micheal Hickerson <mikehickerson@gmail.com>
2018-11-27 17:30:32 -0500

    Add Agile values to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,12 +6,17 @@ import Image from '../components/image'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
+    <h1>Manifesto for Agile Software Development</h1>
+    <p>Reprinted from <a href="https://agilemanifesto.org">AgileManifesto.org</a>.</p>
+    <p>We are uncovering better ways of developing software by doing it and helping others do it.</p>
+    <p>Through this work we have come to value:</p>
+    <ul>
+      <li><strong>Individuals and interactions</strong> over processes and tools</li>
+      <li><strong>Working software</strong> over comprehensive documentation</li>
+      <li><strong>Customer collaboration</strong> over contract negotiation</li>
+      <li><strong>Responding to change</strong> over following a plan</li>
+    </ul>
+    <p>That is, while there is value in the items on the right, we value the items on the left more.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
