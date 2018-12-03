b8ff1b1270900020c6ab0dfba423c2016a1738a7
Micheal Hickerson <mikehickerson@gmail.com>
2018-12-03 11:05:47 -0500

    Structure manifesto text

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,55 +5,62 @@ import Layout from '../components/layout'
 
 const IndexPage = ({ data }) => (
   <Layout>
-    <h1>Manifesto for Agile Software Development</h1>
-    <p>
-      Reprinted from <a href="https://agilemanifesto.org">AgileManifesto.org</a>
-      .
-    </p>
-    <p>
-      We are uncovering better ways of developing software by doing it and
-      helping others do it.
-    </p>
-    <p>Through this work we have come to value:</p>
-    <ul>
-      {data.allValuesYaml.edges.map(({ node }, index) => (
-        <li>
-          <strong>{node.greaterValue}</strong> over {node.lesserValue}
-        </li>
-      ))}
-    </ul>
-    <p>
-      That is, while there is value in the items on the right, we value the
-      items on the left more.
-    </p>
-    <ul>
-    {data.allSignersYaml.edges.map(({ node }, index) => (
-        <li>{node.name}</li>
-      ))}
-    </ul>
-    <Link to="/principles/">Twelve Principles of Agile Software</Link>
+    <section className="section">
+      <div className="container">
+      <h1 className="title">Manifesto for Agile Software Development</h1>
+      <h2 className="subtitle">
+        Reprinted from{' '}
+        <a href="https://agilemanifesto.org">AgileManifesto.org</a>.
+      </h2>
+      <div className="content">
+        <p>
+          We are uncovering better ways of developing software by doing it and
+          helping others do it.
+        </p>
+        <p>Through this work we have come to value:</p>
+        <ul>
+          {data.allValuesYaml.edges.map(({ node }, index) => (
+            <li>
+              <strong>{node.greaterValue}</strong> over {node.lesserValue}
+            </li>
+          ))}
+        </ul>
+        <p>
+          That is, while there is value in the items on the right, we value the
+          items on the left more.
+        </p>
+      </div>
+      <ul>
+        {data.allSignersYaml.edges.map(({ node }, index) => (
+          <li>{node.name}</li>
+        ))}
+      </ul>
+      <Link to="/principles/">Twelve Principles of Agile Software</Link>
+      </div>
+    </section>
   </Layout>
 )
 
 export const query = graphql`
-query HomePageQuery {
-  allSignersYaml {
-  edges {
-    node {
-      id
-      name
+  query HomePageQuery {
+    allSignersYaml {
+      edges {
+        node {
+          id
+          name
+        }
+      }
+    }
+    allValuesYaml {
+      edges {
+        node {
+          id
+          greaterValue
+          lesserValue
+        }
+      }
     }
   }
-}
-allValuesYaml {
-  edges {
-    node {
-      id
-      greaterValue
-      lesserValue
-    }
-  }
-}}
 `
 
 export default IndexPage
