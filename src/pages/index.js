5bb32fddd75beffdfa1a010125d42ad7bd3a183e
Micheal Hickerson <mikehickerson@gmail.com>
2018-12-03 11:34:21 -0500

    Separate Signers into component

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,19 +3,14 @@ import { Link, graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 import Manifesto from '../components/manifesto'
+import Signers from '../components/signers'
 
 const IndexPage = ({ data }) => (
   <Layout>
     <section className="section">
       <Manifesto values={data.allValuesYaml.edges}></Manifesto>
-      <div className="container">
-      <ul>
-        {data.allSignersYaml.edges.map(({ node }, index) => (
-          <li>{node.name}</li>
-        ))}
-      </ul>
+      <Signers signers={data.allSignersYaml.edges}></Signers>
       <Link to="/principles/">Twelve Principles of Agile Software</Link>
-      </div>
     </section>
   </Layout>
 )
