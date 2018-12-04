21c2ee1a14a52d08dd0bcad102870c0da8c1a76e
Micheal Hickerson <mikehickerson@gmail.com>
2018-12-04 12:11:04 -0500

    Update manifesto.js

--- src/components/manifesto.js
+++ src/components/manifesto.js
@@ -4,22 +4,28 @@ const Manifesto = ({ values }) => (
   <div className="container">
     <h1 className="title">Manifesto for Agile Software Development</h1>
     <h2 className="subtitle">
-      Reprinted from <a href="https://agilemanifesto.org">AgileManifesto.org</a>
-      .
+      Reprinted from <a href="https://agilemanifesto.org">AgileManifesto.org</a>.
     </h2>
-    <div className="content">
+    <div className="box is-size-5">
       <p>
         We are uncovering better ways of developing software by doing it and
         helping others do it.
       </p>
       <p>Through this work we have come to value:</p>
-      <ul>
+      <div className="section">
         {values.map(({ node }, index) => (
-          <li>
-            <strong>{node.greaterValue}</strong> over {node.lesserValue}
-          </li>
+          <div className="level" key={node.index}>
+            <div className="level-left">
+              <div className="level-item">
+                <strong>{node.greaterValue}</strong>
+              </div>
+            </div>
+            <div className="level-right">
+              <div className="level-item">over {node.lesserValue}</div>
+            </div>
+          </div>
         ))}
-      </ul>
+      </div>
       <p>
         That is, while there is value in the items on the right, we value the
         items on the left more.
