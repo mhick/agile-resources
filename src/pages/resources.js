0a751d74d67644067497b7ce51cacf26ed42ded2
Micheal Hickerson <mikehickerson@gmail.com>
2018-12-06 10:02:01 -0500

    Add link to resources page

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -1,2 +1,38 @@
 // TODO: Query for resources matching values and principles
 // TODO: Display resources as cards
+
+import React from 'react'
+import { Link, graphql } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const Resources = ({ data }) => (
+  <Layout>
+    <h1>Twelve Principles of Agile Software</h1>
+    <p>
+      Reprinted from <a href="https://agilemanifesto.org">AgileManifesto.org</a>
+      .
+    </p>
+    <ol>
+      {data.allPrinciplesYaml.edges.map(({ node }, index) => (
+        <li key={node.id}>{node.text}</li>
+      ))}
+    </ol>
+    <Link to="/">Return to Agile Manifesto</Link>
+  </Layout>
+)
+
+export const query = graphql`
+  query ResourceQuery {
+    allPrinciplesYaml {
+      edges {
+        node {
+          id
+          text
+        }
+      }
+    }
+  }
+`
+
+export default Resources
