d8959fc9c9c6255f46101d72bbbad6e8ff8aae3c
Micheal Hickerson <mikehickerson@gmail.com>
2018-12-06 10:55:02 -0500

    Query resources and value by value number

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -9,12 +9,11 @@ import Layout from '../components/layout'
 const Resources = ({ data }) => (
   <Layout>
     <h1>Resources</h1>
-    <p>
-      Reprinted from <a href="https://agilemanifesto.org">AgileManifesto.org</a>
-      .
-    </p>
+    {data.values.edges.map(({ node }, index) => (
+        <p key={node.id}>{node.greaterValue}</p>
+      ))}
     <ol>
-      {data.allResourcesYaml.edges.map(({ node }, index) => (
+      {data.resources.edges.map(({ node }, index) => (
         <li key={node.id}>{node.name}</li>
       ))}
     </ol>
@@ -23,23 +22,33 @@ const Resources = ({ data }) => (
 )
 
 export const query = graphql`
-  query ResourceQuery {
-    allResourcesYaml(filter: { values: { in: [1] } }) {
-      edges {
-        node {
-          id
-          name
-          author {
-            family
-            given
-          }
-          url
-          values
-          principles
-        }
+query ResourceQuery($searchValue: Int = 1)
+{resources:allResourcesYaml(filter: {values: {in: [$searchValue]}}) {
+  edges {
+    node {
+      id
+      name
+      author {
+        family
+        given
+      }
+      url
+      values
+      principles
+    }
+  }
+}
+  values:allValuesYaml(filter: {value: {eq:$searchValue} },limit:1) {
+    edges {
+      node {
+        id
+        value
+        greaterValue
+        lesserValue
       }
     }
   }
+}
 `
 
 export default Resources
