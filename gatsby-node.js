539decc5d4bfbe78f8acfe71858d2adaf95f40d2
Micheal Hickerson <mikehickerson@gmail.com>
2018-12-07 10:42:15 -0500

    Generate page nodes from values

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,16 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `ValuesYaml`) {
+    const slug = createFilePath({ node, getNode, basePath: `data` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug + node.value,
+    })
+  }
+}
