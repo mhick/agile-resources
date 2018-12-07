7ac6adbed01b35c94c06cd29300f49a461564edb
Micheal Hickerson <mikehickerson@gmail.com>
2018-12-07 10:50:53 -0500

    Create nodes for principles

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,12 +8,22 @@ const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
+  // Create slug for each Value
   if (node.internal.type === `ValuesYaml`) {
     const slug = createFilePath({ node, getNode, basePath: `data` })
     createNodeField({
       node,
       name: `slug`,
       value: slug + node.value,
+    })
+  }
+  // Create slug for each Principle
+  if (node.internal.type === `PrinciplesYaml`) {
+    const slug = createFilePath({ node, getNode, basePath: `data` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug + node.principle,
     })
   }
 }
